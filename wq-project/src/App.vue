8f5162f669e3264b112d3da874ba73adef04dbf4
wangqing <wangqing@shubidatech.com>
2020-06-01 15:07:50 +0800

    first commit

---
+++ wq-project/src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+page{
+  width:100%;
+  height:100%;
+  background:#f7f7f7
+}
+</style>
---
+++ wq-project/src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+   study mpvue
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
